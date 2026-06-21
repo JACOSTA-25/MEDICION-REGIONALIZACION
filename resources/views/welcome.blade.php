913b3b6bd84a44a6bdbf65f64691ba64877e729b
ZapataMor <luiszapataa2017@gmail.com>
2026-06-21 04:11:03 -0500

    Ícono de prestaña añadido.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,8 @@
 
         <title>{{ __('Bienvenido') }} - {{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/webp" href="{{ asset('images/logo-uniguajira-seo-150x150.webp') }}">
+        <link rel="apple-touch-icon" href="{{ asset('images/logo-uniguajira-seo-150x150.webp') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
